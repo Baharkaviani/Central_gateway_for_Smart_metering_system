e647b7fdcebc3adeb6bae0972345cef83d42d014
baharKaviani <bhr.kaviani@gmail.com>
2023-10-01 14:00:02 +0330

    02-07-09 (feat) send triple data

--- src/send_simulated_data/src/src.ino
+++ src/send_simulated_data/src/src.ino
@@ -11,51 +11,53 @@ SoftwareSerial S(RX, TX);
 ModbusRTU mb;
 
 // Define your data to be sent as an array
-uint16_t dataToSend[] = {100, 200};
+uint16_t dataToSend[3] = {123, 234, 345};
 
 // Define the Modbus slave address
 const uint8_t slaveAddress = 1;  // Change this to the address of your Modbus slave
 
 float InttoFloat(uint16_t Data0,uint16_t Data1) {
-  float x;
-  unsigned long *p;
-  p = (unsigned long*)&x;
-  *p = (unsigned long)Data0 << 16 | Data1; //Big-endian
-  return(x);
+    float x;
+    unsigned long *p;
+    p = (unsigned long*)&x;
+    *p = (unsigned long)Data0 << 16 | Data1; //Big-endian
+    return(x);
 }
 
 bool cbWrite(Modbus::ResultCode event, uint16_t transactionId, void* data) {
-  Serial.printf_P("Request result: 0x%02X, Mem: %d\n", event, ESP.getFreeHeap());
-  return true;
+    Serial.printf_P("Request result: 0x%02X, Mem: %d\n", event, ESP.getFreeHeap());
+    return true;
 }
 
 void setup() {
-  pinMode(DE_RE, OUTPUT);
-  // Initialize the ModbusRTU communication with the specified Serial port
-  Serial.begin(115200);  // Set the baud rate to match your RS485 network configuration
-  S.begin(9600, SWSERIAL_8N1);
-  mb.begin(&S, DE_RE);
-  mb.master();
+    pinMode(DE_RE, OUTPUT);
+    // Initialize the ModbusRTU communication with the specified Serial port
+    Serial.begin(115200);  // Set the baud rate to match your RS485 network configuration
+    S.begin(9600, SWSERIAL_8N1);
+    mb.begin(&S, DE_RE);
+    mb.master();
 
-  // Set the Modbus slave address
-  mb.slave(slaveAddress);
+    // Set the Modbus slave address
+    mb.slave(slaveAddress);
 }
 
 void loop() {
-  digitalWrite(DE_RE, LOW);
-  // Send data to the Modbus slave using the "mb.writeHreg" function
-  mb.writeHreg(slaveAddress, 1, dataToSend[1], 0);  // Write to register 0
-  // mb.writeHreg(slaveAddress, 2, dataToSend[1], 0);  // Write to register 1
+    digitalWrite(DE_RE, LOW);
+    // Send data to the Modbus slave using the "mb.writeHreg" function
+    // Serial.println("slaveAddress, 0, dataToSend[0]");
+    // mb.writeHreg(slaveAddress, 0, dataToSend[0]);  // Write to register 0
+    // Serial.println(dataToSend[0]);
+    // Serial.println("=====dataToSend[0] ended=====");
+    // mb.task(); delay(1000); yield();
+    Serial.println("slaveAddress, 0, &dataToSend[0], 3");
+    mb.writeHreg(slaveAddress, 0, &dataToSend[0], 3);  // Write to register 0
+    Serial.println("=====dataToSend[0 , 1 , 2] ended=====");
 
-  if (!mb.slave()) {
-    mb.writeHreg(slaveAddress, 2699, dataToSend[0], 0);
-    // mb.writeHreg(1, 2699, dataToSend[0], 2);
-    Serial.println(InttoFloat(dataToSend[0],  dataToSend[1]));
-  }
-  mb.task();
+    mb.task();
 
-  // Delay before sending data again (adjust as needed)
-  delay(1000);
-
-  yield();
+    // Delay before sending data again (adjust as needed)
+    delay(1000);
+    yield();
+    digitalWrite(DE_RE, HIGH);
+    delay(500);
 }

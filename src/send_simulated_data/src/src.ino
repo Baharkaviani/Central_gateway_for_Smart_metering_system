07b1edb6ea15bbd1ab642c991566a5a76b87d09c
baharKaviani <bhr.kaviani@gmail.com>
2023-10-01 14:59:46 +0330

    02-07-09 (feat) update data with random number

--- src/send_simulated_data/src/src.ino
+++ src/send_simulated_data/src/src.ino
@@ -14,50 +14,55 @@ ModbusRTU mb;
 uint16_t dataToSend[3] = {123, 234, 345};
 
 // Define the Modbus slave address
-const uint8_t slaveAddress = 1;  // Change this to the address of your Modbus slave
+const uint8_t slaveAddress = 1;
 
-float InttoFloat(uint16_t Data0,uint16_t Data1) {
-    float x;
-    unsigned long *p;
-    p = (unsigned long*)&x;
-    *p = (unsigned long)Data0 << 16 | Data1; //Big-endian
-    return(x);
-}
-
-bool cbWrite(Modbus::ResultCode event, uint16_t transactionId, void* data) {
-    Serial.printf_P("Request result: 0x%02X, Mem: %d\n", event, ESP.getFreeHeap());
-    return true;
-}
+long gasRandNumber = 0;
+long waterRandNumber = 0;
+long powerRandNumber = 0;
 
 void setup() {
     pinMode(DE_RE, OUTPUT);
     // Initialize the ModbusRTU communication with the specified Serial port
-    Serial.begin(115200);  // Set the baud rate to match your RS485 network configuration
+    Serial.begin(115200);
+
+    // Set the baud rate to match your RS485 network configuration
     S.begin(9600, SWSERIAL_8N1);
     mb.begin(&S, DE_RE);
     mb.master();
 
     // Set the Modbus slave address
     mb.slave(slaveAddress);
+
+    // if analog input pin 0 is unconnected, random analog
+    // noise will cause the call to randomSeed() to generate
+    // different seed numbers each time the sketch runs.
+    // randomSeed() will then shuffle the random function.
+    randomSeed(analogRead(0));
 }
 
 void loop() {
     digitalWrite(DE_RE, LOW);
-    // Send data to the Modbus slave using the "mb.writeHreg" function
-    // Serial.println("slaveAddress, 0, dataToSend[0]");
-    // mb.writeHreg(slaveAddress, 0, dataToSend[0]);  // Write to register 0
-    // Serial.println(dataToSend[0]);
-    // Serial.println("=====dataToSend[0] ended=====");
-    // mb.task(); delay(1000); yield();
-    Serial.println("slaveAddress, 0, &dataToSend[0], 3");
     mb.writeHreg(slaveAddress, 0, &dataToSend[0], 3);  // Write to register 0
-    Serial.println("=====dataToSend[0 , 1 , 2] ended=====");
-
+    Serial.print("gas data: ");
+    Serial.println(dataToSend[0]);
+    Serial.print("water data: ");
+    Serial.println(dataToSend[1]);
+    Serial.print("power data: ");
+    Serial.println(dataToSend[2]);
+    Serial.println("====================");
     mb.task();
-
-    // Delay before sending data again (adjust as needed)
-    delay(1000);
-    yield();
+    delay(100); // Delay before sending data again
     digitalWrite(DE_RE, HIGH);
-    delay(500);
+
+    yield();
+
+    // generate random numbers from 10 to 20
+    gasRandNumber = random(10, 21);
+    waterRandNumber = random(10, 21);
+    powerRandNumber = random(10, 21);
+    
+    dataToSend[0] = dataToSend[0] + gasRandNumber;
+    dataToSend[1] = dataToSend[1] + waterRandNumber;
+    dataToSend[2] = dataToSend[2] + powerRandNumber;
+    delay(1000);
 }
